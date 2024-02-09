2890365f82af85615cd1eaf158add4b74693ed02
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-08 20:54:15 -0800

    updated the script to install the test harnesses for TDD

--- setup.sh
+++ setup.sh
@@ -4,3 +4,33 @@
 # Description: Helps to setup the needed development packages.
 
 
+install_software () {
+    echo "Installing needed dependencies..."
+    sudo apt install build-essential
+    sudo apt install cmake
+    sudo apt install make
+    sudo apt install git
+}
+
+# Installs the CXX(cpp) unit testing framework
+install_cpputest () {
+    sudo apt install cpputest
+}
+
+# Builds and installs CMocka the unit testing harness.
+install_cmocka () {
+    echo "Downloading CMOCKA unit testing harness..."
+    git clone https://git.cryptomilk.org/projects/cmocka.git
+    cd cmocka
+    echo "Building CMOCKA..."
+    cmake -S . -B build -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build
+    echo "cmocka should be ready now."
+}
+
+
+
+install_software
+#install_cpputest
+install_cmocka
+
