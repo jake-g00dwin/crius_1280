520000b1a57e487d33d77bae390fc2acff261b0e
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-10 09:46:47 -0800

    updated with variables and functions

--- tdd.sh
+++ tdd.sh
@@ -2,9 +2,9 @@
 
 CROSS_TC="$(pwd)/aarch64_toolchain.cmake"
 CMAKE_VERBOSE="ON"
+CROSS_COMPILE=1
 
 echo $CROSS_TC
-
 
 
 clear_cmake_cache () {
@@ -14,7 +14,7 @@ clear_cmake_cache () {
 
 build_calibration_example () {
     clear_cmake_cache
-
+    
     cmake -DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE} -DCMAKE_TOOLCHAIN_FILE=${CROSS_TC} ../
     make calibrationExample
 }
@@ -26,4 +26,28 @@ build_example_smart_ir () {
     make calibrationExample
 }
 
-build_calibration_example 
+
+build_main () {
+    clear_cmake_cache
+    
+    cmake -DCMAKE_VERBOSE_MAKEFILE=${CMAKE_VERBOSE} ../
+    make main
+}
+
+build_main_test () {
+    clear_cmake_cache
+    
+    cmake -DUNIT_TESTING=1 ../
+    ctest
+}
+
+
+build_pgm_test ()  {
+    clear_cmake_cache
+    cmake -DUNIT_TESTING=1 ../
+    ctest
+}
+
+build_pgm_test
+#build_calibration_example 
+
