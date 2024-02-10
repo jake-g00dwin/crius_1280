771682bba38b28c9cbd024a30f5e022559cb1628
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-10 11:15:19 -0800

    added test for existance of the pgm structure

--- tests/pgm/test_pgm.c
+++ tests/pgm/test_pgm.c
@@ -21,6 +21,16 @@
  */
 
 
+/*Test case for ensuring the correct structures exist.*/
+static void test_pgm_struct(void **state) {
+    (void) state;
+
+    pgm_t my_pgm_img;
+    my_pgm_img.val = 1;
+    assert_true(my_pgm_img.val);
+}
+
+
 /* A test case that does nothing and succeeds. */
 static void null_test_success(void **state) {
     (void) state; /* unused */
@@ -30,6 +40,7 @@ int main(void)
 {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(null_test_success),
+        cmocka_unit_test(test_pgm_struct),
     }; 
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
