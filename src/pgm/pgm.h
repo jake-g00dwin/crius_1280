a4e939c1920151e29fb3598c22033c1b8c3be91f
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-10 11:26:16 -0800

    added constatnts to define the maximum width and height for our matrix.

--- src/pgm/pgm.h
+++ src/pgm/pgm.h
@@ -2,11 +2,22 @@
 #define PGM 
 
 #include <inttypes.h>
+#include <stddef.h>
 #include <stdint.h>
 
+#define MAX_2D_COLS 1920
+#define MAX_2D_ROWS 1080 
+
 typedef struct {
+    uint16_t width;
+    uint16_t height;
     uint8_t val;
 }pgm_t;
+
+typedef struct {
+    size_t x; //The number of cols
+    size_t y; //The number of rows 
+}matrix_t;
 
 void self_test(void);
 
