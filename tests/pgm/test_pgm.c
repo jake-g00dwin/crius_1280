70aefa71e72876cc83a830d716dc0fc3d2388832
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-10 11:43:40 -0800

    wrote test to check that clearing the matrix works

--- tests/pgm/test_pgm.c
+++ tests/pgm/test_pgm.c
@@ -32,7 +32,12 @@ static void test_pgm_struct(void **state) {
     my_pgm_img.width = 1920;
     my_pgm_img.height = 1080;
 
-    //Now try to access the 2d data array.  
+    my_pgm_img.data_matrix.x = 1920;
+    my_pgm_img.data_matrix.y = 1080;
+
+
+    //Now try to access the 2d data array. 
+    assert_non_null(my_pgm_img.data_matrix.x);
 }
 
 /*Test case for the 2D matrix implimentation.*/
@@ -44,6 +49,23 @@ static void test_2dmatrix_struct(void **state)
 
     assert_in_range(my_matrix.x, 0, MAX_2D_COLS);
     assert_in_range(my_matrix.y, 0, MAX_2D_ROWS);
+
+}
+
+static void test_clear_matrix(void **state)
+{
+    matrix_t my_matrix;
+
+    my_matrix.x = 1920;
+    my_matrix.y = 1080;
+    
+    clear_matrix(&my_matrix);
+    
+    for(int row = 0; row < MAX_2D_ROWS; row++) {
+        for(int col = 0; col < MAX_2D_COLS; col++){
+            assert_true(my_matrix.data[col][row] == 0);
+        }
+    }
 }
 
 
@@ -58,6 +80,7 @@ int main(void)
         cmocka_unit_test(null_test_success),
         cmocka_unit_test(test_pgm_struct),
         cmocka_unit_test(test_2dmatrix_struct),
+        cmocka_unit_test(test_clear_matrix),
     }; 
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
