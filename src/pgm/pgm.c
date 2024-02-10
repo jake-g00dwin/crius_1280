dd2121a7d3a87add1dbcc5fc96c3a565aebba684
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-10 11:54:50 -0800

    added code to pass the tests for the new pmg function.

--- src/pgm/pgm.c
+++ src/pgm/pgm.c
@@ -1,15 +1,19 @@
 #include "pgm.h"
-#if defined (__linux__)
-#include <pthread.h>
-#include <unistd.h>
-#endif
 
+#include <stdio.h>
+#include <stdlib.h>
 
 void self_test(void)
 {
     //do nothing
     return;
 }
+
+/*
+ * ############################
+ * MATRIX HELPERS/FUNCS 
+ * ############################
+ */ 
 
 void clear_matrix(matrix_t *m)
 {
@@ -19,4 +23,18 @@ void clear_matrix(matrix_t *m)
         } 
     }
      
+}
+
+/*
+ * ############################
+ * PGM(PORTABLE GRAY FORMAT) Img 
+ * ############################
+ */ 
+
+pgm_t new_pgm_image(size_t width, size_t height) {
+    pgm_t img;
+    img.height = height;
+    img.width = width;
+    clear_matrix(&img.data_matrix);
+    return img; 
 }
