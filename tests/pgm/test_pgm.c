3c59c0e3ef6eea7dc7b9bf567c10f54f3b8b89c5
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-10 11:26:33 -0800

    added tests to check for the correct range of the matrix structure

--- tests/pgm/test_pgm.c
+++ tests/pgm/test_pgm.c
@@ -28,6 +28,22 @@ static void test_pgm_struct(void **state) {
     pgm_t my_pgm_img;
     my_pgm_img.val = 1;
     assert_true(my_pgm_img.val);
+    
+    my_pgm_img.width = 1920;
+    my_pgm_img.height = 1080;
+
+    //Now try to access the 2d data array.  
+}
+
+/*Test case for the 2D matrix implimentation.*/
+static void test_2dmatrix_struct(void **state)
+{
+    matrix_t my_matrix;
+    my_matrix.x = 1920;
+    my_matrix.y = 1080;
+
+    assert_in_range(my_matrix.x, 0, MAX_2D_COLS);
+    assert_in_range(my_matrix.y, 0, MAX_2D_ROWS);
 }
 
 
@@ -41,6 +57,7 @@ int main(void)
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(null_test_success),
         cmocka_unit_test(test_pgm_struct),
+        cmocka_unit_test(test_2dmatrix_struct),
     }; 
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
