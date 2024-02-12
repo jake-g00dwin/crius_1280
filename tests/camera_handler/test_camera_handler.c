f36a8c5c1fb676aec58b49496343620df19fd8d4
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-12 11:31:19 -0800

    wrote test to check for valid fps

--- tests/camera_handler/test_camera_handler.c
+++ tests/camera_handler/test_camera_handler.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <setjmp.h>
@@ -22,21 +23,41 @@
 
 /*
  * ############################
- * Tests for PGM
+ * Tests for SDK interface. 
  * ############################
- */ 
+ */
+
+static void test_camera_init(void **state) {
+    int fps = 5;
+    bool SL = false;
+    bool BP = false;
+    enum agc AGC = no_agc; 
+    int result = init_camera(fps, SL, BP, AGC);
+    assert_true(result == 0);
+
+    fps = 0;
+    result = init_camera(fps, SL, BP, AGC);
+    assert_false(result == 0);
+
+    fps = 175;
+    result = init_camera(fps, SL, BP, AGC);
+    assert_false(result == 0);
+
+   
+} 
+
 
 /* A test case that does nothing and succeeds. */
 static void null_test_success(void **state) {
     (void) state; /* unused */
 }
 
-
 int main(void)
 {
      
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(null_test_success),
+        cmocka_unit_test(test_camera_init),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
