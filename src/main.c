2b0e5ce7bdea920eaa59a0563e3d02154be00100
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-12 10:24:54 -0800

    moved the old main.c functionality into a rawtopgm.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
+/*
+ * Author: Jake G
+ * Date: 2023
+ * filename: main.c
+ * Description: Acts as interface to C API for camera for python calls.
+ */
+
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -9,28 +16,5 @@
 
 int main() {
     printf("CMAKE FUNCTIONAL\r\n");
-   
-
-    //char src[] = "frame0.raw";
-    char src[] = "imgRAW.bin";
-    char dst[] = "image.pgm";
-    pgm_t frame = new_pgm_image(1280, 1024);
-    frame.height = 1024;
-    frame.width = 1280;
-
-    size_t num_bytes = sizeof(uint16_t) * 1280 * 1024;
-    uint16_t* file_contents = malloc(num_bytes);
-
-    /*Open the raw data.*/
-    int fd = open(src, O_RDONLY);
-   
-    read(fd, file_contents, num_bytes);
-
-    parse_raw_data(&frame, file_contents);
-    save_pgm_image(&frame, dst);
-
-    close(fd);
-    free(file_contents);
-
     return 0;
 }
---
+++ src/rawtopgm.c
@@ -0,0 +1,42 @@
+/*
+ * Author: Jake G
+ * Date: 2023
+ * filename: rawtopgm.c
+ * Description: converts raw binary camera files in little-endian into pgm files.
+ */
+
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "pgm.h"
+
+
+int main() {
+    printf("CMAKE FUNCTIONAL\r\n");
+   
+
+    char src[] = "imgRAW.bin";
+    char dst[] = "image.pgm";
+    pgm_t frame = new_pgm_image(1280, 1024);
+    frame.height = 1024;
+    frame.width = 1280;
+
+    size_t num_bytes = sizeof(uint16_t) * 1280 * 1024;
+    uint16_t* file_contents = malloc(num_bytes);
+
+    /*Open the raw data.*/
+    int fd = open(src, O_RDONLY);
+   
+    read(fd, file_contents, num_bytes);
+
+    parse_raw_data(&frame, file_contents);
+    save_pgm_image(&frame, dst);
+
+    close(fd);
+    free(file_contents);
+
+    return 0;
+}
