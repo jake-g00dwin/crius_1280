68a2d402402f75a754821cad3a9e6c4bdc0a2250
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-12 12:24:44 -0800

    added code to pass the tests for the init_camera() func

--- src/camera_handler/camera_handler.c
+++ src/camera_handler/camera_handler.c
@@ -1,14 +1,12 @@
 #include "camera_handler.h"
 
-#include <stdbool.h>
-#include <stdint.h>
-
 #include "DALProxy1280_12USB.h"
 #include "DALProxy1280_12USBDef.h"
 #include "DALProxySwitchUSBDef.h"
 
 
-//uint16_t paImage[IRIMAGE_NBPIXELS*2];
+
+uint16_t paImage[IRIMAGE_NBPIXELS*2];
 
 
 int init_camera(uint8_t fps, bool SL, bool BP, uint8_t agc)
@@ -16,8 +14,21 @@ int init_camera(uint8_t fps, bool SL, bool BP, uint8_t agc)
     if(fps > MAX_FPS || fps < MIN_FPS) {
         return -1;
     }
-
-
+    
+   switch(agc){
+        case no_agc:
+                break;
+        case hist:
+                break;
+        case local:
+                break;
+        case linear:
+                break;
+        default:
+                return -1;
+                break;
+    }
+        
     return 0;
 }
 
--- src/camera_handler/camera_handler.h
+++ src/camera_handler/camera_handler.h
@@ -6,6 +6,7 @@
 //#define IRIMAGE_NBPIXELS 1920 * 1080
 //#endif
 
+
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -22,10 +23,17 @@ enum agc {
 /*SL shutterless processing*/
 /*BP Bad pixels corrections*/
 
+//put the object type for python export here.
+
 
 int cam_self_test(void);
 int init_camera(uint8_t fps, bool SL, bool BP, uint8_t agc);
-void get_frame();
+int close_camera();
+
+void get_pgm_frame();
+void get_frame_matrix();
+void get_meta_data();
+void chagne_parameters();
 
 
 #endif
