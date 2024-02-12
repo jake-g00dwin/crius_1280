029cdd7da2e92b8e2a27ccb0b176af31fe40ced0
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-12 15:26:15 -0800

    refactored into functions

--- src/pyprog/cam.py
+++ src/pyprog/cam.py
@@ -6,21 +6,37 @@ from ctypes import c_size_t, c_double
 # from ctypes import *
 import numpy as np
 
-# load the library
-# mylib = CDLL("mylib.so")
-mylib = CDLL("./mylib.so")
 
-# C-type corresponding to numpy array
-ND_POINTER_1 = np.ctypeslib.ndpointer(dtype=np.float64,
-                                      ndim=1,
-                                      flags="C")
+def quicktest():
+    # load the library
+    # mylib = CDLL("mylib.so")
+    mylib = CDLL("./mylib.so")
 
-# define prototypes
-mylib.print_array.argtypes = [ND_POINTER_1, c_size_t]
-mylib.print_array.restype = None
+    # C-type corresponding to numpy array
+    ND_POINTER_1 = np.ctypeslib.ndpointer(dtype=np.float64,
+                                          ndim=1,
+                                          flags="C")
 
-# create array X = [1 1 1 1 1]
-X = np.ones(5)
-# call function
-mylib.print_array(X, X.size)
+    # define prototypes
+    mylib.print_array.argtypes = [ND_POINTER_1, c_size_t]
+    mylib.print_array.restype = None
 
+    # create array X = [1 1 1 1 1]
+    X = np.ones(5)
+    # call function
+    mylib.print_array(X, X.size)
+
+
+def main():
+    camlib = CDLL("./camera_handler.so")
+    # Pass in args to the C function to get a camera 'HANDLE' type.
+    camera_handle = camlib.init_camera()
+
+    # Now request frame/image from the camera that's formatted.
+    ND_POINTER_1 = np.ctypeslib.ndpointer(dtype=np.uint16, ndim=2, flags="C")
+
+    #define prototypes camlib
+    camlib.get_frame(ND_POINTER_1, c_size_t)
+    camlib.get_fame.restype = None
+
+main
