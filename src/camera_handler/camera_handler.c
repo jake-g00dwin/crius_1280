75ba60976040c3f38372068bf3d46ce063bfbf96
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-11 21:01:53 -0800

    started on the module for handling the camera itself.

--- src/camera_handler/camera_handler.c
+++ src/camera_handler/camera_handler.c
@@ -1,5 +1,14 @@
 #include "camera_handler.h"
 
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "DALProxy1280_12USB.h"
+#include "DALProxy1280_12USBDef.h"
+#include "DALProxySwitchUSBDef.h"
+
+
+uint16_t paImage[IRIMAGE_NBPIXELS*2];
 
 int cam_self_test(void)
 {
--- src/camera_handler/camera_handler.h
+++ src/camera_handler/camera_handler.h
@@ -1,7 +1,14 @@
 #ifndef CAM_HANDLER
 #define CAM_HANDLER
 
+/*This should represent the number of pixels*/
+//#ifndef IRIMAGE_NBPIXELS
+//#define IRIMAGE_NBPIXELS 1920 * 1080
+//#endif
+
 int cam_self_test(void);
+
+void get_frame();
 
 
 #endif
