6e8a2d4de53f7f9a3f0ea46c17bd71ed5c0c0127
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-13 19:40:34 -0800

    Set it to default to using SL

--- src/pyprog/cam.py
+++ src/pyprog/cam.py
@@ -68,7 +68,7 @@ def main():
     print("camlib.num_attached(): " + str(number_modules))
 
     handle = c_long()
-    handle = camlib.init_camera(1, False, 1, 2, 1)
+    handle = camlib.init_camera(1, True, 1, 2, 1)
     print("camlib.init_camera(): " + str(handle))
 
     # show that we can get camera frames.
