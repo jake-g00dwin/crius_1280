b7080b7ff176eac7aa45970ccfc92f43bd07ec19
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-15 20:57:25 -0800

    updated to use diffent shared libs for windows build

---
+++ i686-w64-mingw32_toolchain.cmake
@@ -0,0 +1,20 @@
+# Specify the cross compiler
+SET(CMAKE_C_COMPILER /usr/bin/i686-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-gcc)
+
+#SET(target_tripple "i686-w64-mingw32")
+#SET(cpu_flags "")
+
+# Specify the target architecture
+SET(CMAKE_SYSTEM_NAME MINGW)
+#SET(CMAKE_SYSTEM_PROCESSOR X86)
+SET(CMAKE_SYSTEM_PROCESSOR AMD64)
+
+# Specify the root directory of the cross-compiler toolchain
+SET(CMAKE_FIND_ROOT_PATH /usr/bin/) 
+
+# Specify the search path for libraries and headers
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
