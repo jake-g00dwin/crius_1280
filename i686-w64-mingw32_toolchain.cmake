07dc903475f7f609c0383b90882a0e6db9de54f2
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-16 01:08:54 -0800

    added a toolchain file for crosscompiling for windows systems.

--- i686-w64-mingw32_toolchain.cmake
+++ i686-w64-mingw32_toolchain.cmake
@@ -1,12 +1,15 @@
 # Specify the cross compiler
 SET(CMAKE_C_COMPILER /usr/bin/i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-gcc)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(CMAKE_RANLIB i686-w64-mingw32-ranlib)
 
 #SET(target_tripple "i686-w64-mingw32")
 #SET(cpu_flags "")
 
 # Specify the target architecture
-SET(CMAKE_SYSTEM_NAME MINGW)
+#SET(CMAKE_SYSTEM_NAME MINGW)
+SET(CMAKE_SYSTEM_NAME Windows)
 #SET(CMAKE_SYSTEM_PROCESSOR X86)
 SET(CMAKE_SYSTEM_PROCESSOR AMD64)
 
