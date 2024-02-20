ef33a1199add148eee6c2d19e636cd51045af6eb
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-20 14:27:31 -0800

    built working version of the video function that maps the 16bit grayscale into RGB values

--- src/pyprog/cam.py
+++ src/pyprog/cam.py
@@ -17,8 +17,16 @@ HEIGHT = 1024
 NUMPIXELS = 1310720
 MIRROR_FRAME = True
 
+window = 'data'
+contrast = 1
+max_contrast = 100
+brightness = 0
+max_brightness = 100
 
-img = np.ones(NUMPIXELS, dtype=np.uint8).reshape(HEIGHT, WIDTH, order="C")
+
+img = np.ones(NUMPIXELS, dtype=np.uint16).reshape(HEIGHT, WIDTH, order="C")
+im1 = None
+# img = np.float32(img/255)
 
 # Handle the OS specific shared library.
 if(platform.system() == "Linux"):
@@ -40,7 +48,7 @@ class eAGC(Enum):
 
 # Moved defaults into global scope for use in demo functions
 DEF_FPS = 60.0
-DEF_AGC = eAGC.agc_eq_history.value
+DEF_AGC = eAGC.agc_linear.value
 DEF_SL = 1
 DEF_BP = 1
 DEF_NUC = 1
@@ -222,20 +230,10 @@ def demo_video(set_8bit):
         load_frame_buffer(h)
         load_matrix_buffer(False)
         img = get_frame_matrix()
+        cv.cvtColor(img, cv.COLOR_GRAY2RGB, im1)
 
-        if(set_8bit):
-            img = cv.normalize(
-                    img,
-                    None,
-                    0,
-                    255,
-                    cv.NORM_MINMAX).astype(np.uint8)
-        else:
-            img = (img/256).astype('uint8')
-            newmat = np.ones(NUMPIXELS, dtype=np.uint8).reshape(HEIGHT, WIDTH, order="C")
-            cv.equalizeHist(img, newmat)
-            img = newmat
-        cv.imshow('data', img)
+        cv.imshow(window, im1)
+
         key = cv.waitKey(16)
 
         if key == ord('q'):
@@ -244,61 +242,60 @@ def demo_video(set_8bit):
     cv.destroyAllWindows()
     close_camera(h)
 
+
 # Callbacks for the adjustments.
-
-
-contrast = 10
-max_contrast = 100
-brightness = 0
-max_brightness = 100
-
-
 def change_contrast(val):
     global contrast
-    contrast = val / 10
-    preform_operation()
+    contrast = val/10
+    perform_operation()
 
 
 def change_brightness(val):
     global brightness
-    brightness = val / 100
-    preform_operation()
+    brightness = val/100
+    perform_operation()
 
 
-def preform_operation():
-    img1 = (img/255) * contrast + brightness
-    cv.imshow('data', img1)
+def perform_operation():
+    im2 = im1 * contrast + brightness
+    cv.imshow(window, im2)
 
 
 def demo_image(set_8bit):
+    global img
     clear_matrix()
     clear_paimage()
     h = init(fps=DEF_FPS, SL=DEF_SL, BP=DEF_BP, AGC=DEF_AGC, nuc=DEF_NUC)
 
     load_frame_buffer(h)
     load_matrix_buffer(False)
-    mat = get_frame_matrix()
-    global img
     img = get_frame_matrix()
 
     close_camera(h)
 
-    if(set_8bit):
-        mat = cv.normalize(
-                mat,
-                None,
-                0,
-                255,
-                cv.NORM_MINMAX).astype(np.uint8)
+    cv.cvtColor(img, cv.COLOR_GRAY2RGB, im1)
+    while(True):
+        cv.imshow(window, img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
 
-    # mat = (mat/256).astype('uint8')
-    # newmat = np.ones(NUMPIXELS, dtype=np.uint8).reshape(HEIGHT, WIDTH, order="C")
-    # cv.equalizeHist(mat, newmat)
-    # cv.imshow('data', newmat)
-    # mat = mat.astype(np.float32)
-    preform_operation()
-    cv.createTrackbar("Contrast", 'data', contrast, max_contrast, change_contrast)
-    cv.createTrackbar("Brightness", 'data', brightness, max_brightness, change_brightness)
 
+def load_image():
+    global img
+    clear_matrix()
+    clear_paimage()
+    h = init(fps=DEF_FPS, SL=DEF_SL, BP=DEF_BP, AGC=DEF_AGC, nuc=DEF_NUC)
+
+    load_frame_buffer(h)
+    load_matrix_buffer(False)
+    img = get_frame_matrix()
+    close_camera(h)
+    img = np.float32(img/4096)
+
+
+def test_image():
+    cv.namedWindow(window)
+    cv.imshow(window, img)
+    cv.createTrackbar("Contrast", window, contrast, max_contrast, change_contrast)
+    cv.createTrackbar("Brightness", window, brightness, max_brightness, change_brightness)
     cv.waitKey(0)
-    cv.destroyAllWindows()
