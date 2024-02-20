6217c686767d1ae542c98cd4c3359e2b96c85033
jakeg00dwin <jakegoodwin@gorge.works>
2024-02-19 18:42:18 -0800

    added additional choice to the menu

--- src/pyprog/demo.py
+++ src/pyprog/demo.py
@@ -12,17 +12,18 @@ def display_menu():
     print("Menu:")
     print("1. Show Video")
     print("2. Show Image")
+    print("3. Fast Calibration")
     print("0. Quit")
 
 
 def get_choice():
     while True:
         try:
-            choice = int(input("Enter your choice (0/1/2): "))
-            if choice in [0, 1, 2]:
+            choice = int(input("Enter your choice (0-3): "))
+            if choice in [0, 1, 2, 3]:
                 return choice
             else:
-                print("Invalid choice. Please enter 0, 1 or 2.")
+                print("Invalid choice. Please enter a value from 0 to 3")
         except ValueError:
             print("Invalid input. Please enter a number.")
 
@@ -44,6 +45,10 @@ def main():
         elif choice == 2:
             # Run the Image demo
             cam.demo_image(set_8bit)
+        elif choice == 3:
+            h = cam.init()
+            cam.calibrate_camera(h)
+            cam.close_camera(h)
 
 
 main()
